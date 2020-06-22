8ca50fa1be4910266c3a8255d03eddaedb720d05
Santiago Guerra <guerrasantiago19@gmail.com>
2020-06-22 00:48:00 -0500

    Fix Eslint warnings

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 serviceWorker.register();
